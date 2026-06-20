200d2209de19ab8acbfb60a0a616823bf0355829
fatimarqz9 <fam04marquez@gmail.com>
2026-06-20 17:15:35 -0600

    cambiando diseño

--- registro.php
+++ registro.php
@@ -69,6 +69,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <main class="login-main">
 <div class="card" style="max-width: 450px;">
+    <div class="img_index">
+            </div>
         <h2>Crear Cuenta</h2>
         
         <?php if(!empty($error)) { echo "<p class='error'>$error</p>"; } ?>
